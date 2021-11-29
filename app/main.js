b04870583ade8a757b91b61bc3b494a97dd8c104
jdluis <92221396+jdluis@users.noreply.github.com>
2021-11-29 23:40:34 +0000

    add themes,
    i need a littels more configurations

--- app/main.js
+++ app/main.js
@@ -1,1 +1,31 @@
 // Cambiar tema de colores de la pagina.
+
+let switches = document.getElementsByClassName('switch');
+
+let style = localStorage.getItem('style');
+
+if (style == null) {
+  setTheme('white');
+} else {
+  setTheme(style);
+}
+
+for (let i of switches) {
+  i.addEventListener('click', function () {
+    let theme = this.dataset.theme;
+    setTheme(theme);
+  });
+}
+
+function setTheme(theme) {
+  if (theme == 'white') {
+    document.getElementById('switcher-id').href = 'styles//themes/white.css';
+  } else if (theme == 'dark') {
+    document.getElementById('switcher-id').href = 'styles//themes/dark.css';
+  } else if (theme == 'grey') {
+    document.getElementById('switcher-id').href = 'styles//themes/grey.css';
+  } else if (theme == 'purple') {
+    document.getElementById('switcher-id').href = 'styles//themes/purple.css';
+  }
+  localStorage.setItem('style', theme);
+}
